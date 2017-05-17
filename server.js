3fa7490fe0715e0f865608f3f52b939d75b66beb
harish <harish.saini@gwynniebee.com>
2017-05-17 11:41:36 +0530

    changes for openshift deploy

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use('/',indexRoute);
 //handle api responses
 app.use('/api/v1', require('./api')());
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+server_port =  process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+server_ip_address = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
  
 app.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", port " + server_port )
